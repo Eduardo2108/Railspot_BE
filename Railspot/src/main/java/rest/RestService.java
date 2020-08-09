90567d2ecfbaea5bf2b083144ed6d8b39b532fd9
Eduardo <eduardo212792@gmail.com>
2020-08-09 01:20:09 -0600

    Added settings for the project, must use JDK 1.8 and glassfish 5.0.0

---
+++ Railspot/src/main/java/rest/RestService.java
@@ -0,0 +1,16 @@
+package rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/")
+public class RestService extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(Test.class);
+        return classes;
+    }
+}
