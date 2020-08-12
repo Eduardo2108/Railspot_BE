d54678e4dfc00eed5715f1c77e4cef2f43e58fa9
Eduardo <eduardo212792@gmail.com>
2020-08-11 21:03:01 -0600

    Added some javadoc comments.

--- Railspot/src/main/java/main/Railspot.java
+++ Railspot/src/main/java/main/Railspot.java
@@ -1,4 +1,5 @@
-package main ;
+package main;
+
 import backend.Route;
 import backend.Station;
 import util.Graph.Graph;
@@ -8,15 +9,25 @@ import util.Graph.Graph;
  * Driver class for the server, facade for the management of the router, buys and configurations of the server
  */
 public class Railspot {
+    /**
+     * Field for the singleton
+     */
     private static Railspot instance;
+    /**
+     * Field for the stations on a directed graph
+     */
     private Graph<Station> map;
 
-    //constructor
+    /**
+     * Constructor
+     */
     private Railspot() {
         this.map = new Graph<>();
     }
 
-    //singleton
+    /**
+     * singleton
+     */
     public static synchronized Railspot getInstance() {
         if (instance == null) {
             instance = new Railspot();
@@ -24,22 +35,43 @@ public class Railspot {
         return instance;
     }
 
+    /**
+     * Create stations
+     * @param newStation new station to create
+     */
+
     public void createStation(Station newStation) {
+        //todo: convert the type to string and use gson
         this.map.addElement(newStation);
     }
 
     public void deleteStation(Station station) {
+        //todo: convert the type to string and use gson
+        //todo: verify the non existence of any tickets bought
         this.map.deleteElement(station);
     }
 
+    /**
+     * Calculate the shortest path between two stations.
+     * @param a start station
+     * @param b destination
+     * @return route object with the station list and the price of all trip
+     */
     public Route shortestPath(Station a, Station b) {
+        //todo: convert the types to string and use gson
+
         Route route = new Route();
         route.setPath(this.map.shortestPath(a, b));
         return route;
 
     }
 
+    /**
+     * Set the map of the instance to a new one .
+     * @param map
+     */
     public void setMap(Graph<Station> map) {
+        //todo: use this method for loading the graph from the json files.
         this.map = map;
     }
 }
